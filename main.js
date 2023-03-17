9f9128328e60da17de6d671c6107bb537d47e8b5
Kaustubh-Nirmalendu <106333699+Kaustubh-Nirmalendu@users.noreply.github.com>
2023-03-17 11:16:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var canvas = new fabric.Canvas('myCanvas');
      fabric. Image. fromURL ('BirthdayImage.jpg', function (Img) {
      block_image_object = Img;
      block_image_object. scaleToWidth(700);
-     block_image_object. .scaleToHeight(510);
+     block_image_object.scaleToHeight(510);
      block_image_object.set({
      top:0,
      left:0
